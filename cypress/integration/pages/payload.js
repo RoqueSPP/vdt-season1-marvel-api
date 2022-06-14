965143c2f54a86f067db4d75b09f5de451952335
rsppnet <rsppnet@hotmail.com>
2022-06-14 17:34:44 -0300

    Melhoria no teste

---
+++ cypress/integration/pages/payload.js
@@ -0,0 +1,36 @@
+
+        const scenarios = [{
+            'payload': {
+                name: 'Tanos de Titã',
+                team: ['x-man', 'marvel'],
+                active: true
+            },
+            'expected_message': '"alias" is required'
+        },
+        {
+            'payload': {
+                alias: 'Destruidor de planetas',
+                team: ['x-man', 'marvel'],
+                active: true
+            },
+            'expected_message': '"name" is required'
+    
+        },
+        {
+            'payload': {
+                name: 'Tanos de Titã',
+                alias: 'Destruidor de planetas',
+                active: true
+            },
+            'expected_message': '"team" is required'
+        },
+        {
+            'payload': {
+                name: 'Tanos de Titã',
+                alias: 'Destruidor de planetas',
+                team: ['x-man', 'marvel'],
+            },
+            'expected_message': '"active" is required'
+        }
+]
+export default scenarios;
--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,72 +1,20 @@
 
+import scenarios from './pages/payload'
 
 describe('vlidar campos', function () {
     before(function () {
         cy.back2ThePast()
         cy.setToken()
     })
-    it('deve validar o campo name', function () {
-        const payload = {
-            name: "Tanos de Titã",
-            team: ['x-man', 'marvel'],
-            active: true
-        }
-        cy.postCharacter(payload)
-        .then((response) =>{
-            expect(response.status).to.eql(400)
-            expect(response.body.error).to.eql('Bad Request')
-            expect(response.body.validation.body.message).to.eql('"alias" is required')
-                    
-            })
 
-    })
-
-    it('deve validar o campo alias', function () {
-        const payload = {
-            alias: "Destruidor de planetas",
-            team: ['x-man', 'marvel'],
-            active: true
-        }
-        cy.postCharacter(payload)
-        .then((response) =>{
-            expect(response.status).to.eql(400)
-            expect(response.body.error).to.eql('Bad Request')
-            expect(response.body.validation.body.message).to.eql('"name" is required')
-                    
-            })
-
-    })
-
-    it('deve validar o campo team', function () {
-        const payload = {
-            name: "Tanos de Titã",
-            alias: "Destruidor de planetas",
-            active: true
-        }
-        cy.postCharacter(payload)
-        .then((response) =>{
-            expect(response.status).to.eql(400)
-            expect(response.body.error).to.eql('Bad Request')
-            expect(response.body.validation.body.message).to.eql('"team" is required')
-                    
-            })
-
-    })
-    
-    it('deve validar o campo active', function () {
-        const payload = {
-            name: "Tanos de Titã",
-            alias: "Destruidor de planetas",
-            team: ['x-man', 'marvel'],
-        }
-        cy.postCharacter(payload)
-        .then((response) =>{
-            expect(response.status).to.eql(400)
-            expect(response.body.error).to.eql('Bad Request')
-            expect(response.body.validation.body.message).to.eql('"active" is required')
-                    
-            })
-
+    scenarios.forEach(function (scenario) {
+        it('validar campos', function () {
+            cy.postCharacter(scenario.payload)
+                .then(function (response) {
+                    expect(response.status).to.eql(400)
+                    expect(response.body.error).to.eql('Bad Request')
+                    expect(response.body.validation.body.message).to.eql(scenario.expected_message)
+                })
         })
-    
-})
+    })
+    })
